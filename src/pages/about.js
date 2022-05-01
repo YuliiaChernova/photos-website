7ce0c996945b6290814fa23fc52f26bdcb47306a
YuliiaChernova <yuki.chernova.judy@gmail.com>
2022-05-01 12:02:50 +0200

    feature/layout/created layout for header, contact page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Layout from '../components/Layout/';
+import AboutContent from '../components/AboutContent';
+
+const AboutPage = () => (
+  <Layout>
+    <AboutContent />
+  </Layout>
+);
+
+export default AboutPage;
