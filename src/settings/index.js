c5521333cb83014086a9fc74e5e08e429a45ae73
YuliiaChernova <yuki.chernova.judy@gmail.com>
2022-05-12 22:51:51 +0200

    feature/improvments

---
+++ src/settings/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SamplePrevArrow from '../components/SamplePrevArrow';
+
+export const settings = {
+  infinite: true,
+  arrows: true,
+  dots: false,
+  fade: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  swipeToSlide: true,
+  prevArrow: <SamplePrevArrow />,
+  nextArrow: <SamplePrevArrow />,
+};
